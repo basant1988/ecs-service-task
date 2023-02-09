01db4892b33c357270441a7595873eb473d50eb4
basant <basant1988@gmail.com>
2023-02-10 00:16:30 +0530

    ECS cluster and ECR repo

--- ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "jux_consumer_web_repo" {
-  count                = var.environment == "dev" ? 1 : 0
-  provider             = aws.toolsaccount
-  name                 = "${var.projectname}-backend"
-  image_tag_mutability = "MUTABLE"
-  tags                 = var.tags
-}
